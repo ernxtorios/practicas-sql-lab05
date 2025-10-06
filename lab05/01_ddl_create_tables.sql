42a5e8f46dd191bc5e95816b4e57be6614703613
Ernesto Rios <ernestorios@outlook.com.pe>
2025-10-06 15:59:01 -0500

    Update create tables

--- lab05/01_ddl_create_tables.sql
+++ lab05/01_ddl_create_tables.sql
@@ -1,19 +1,28 @@
 USE testing;
 
-CREATE TABLE carreras (
-	id INT,
-	carrera VARCHAR(100),
-	fecha_alta DATETIME,
-	vigente BIT
-);
+DROP TABLE IF EXISTS carreras;
+DROP TABLE IF EXISTS alumnos;
 
-create table alumnos (
-	id INT,
-	nombre VARCHAR(50),
-	apellido VARCHAR(50),
-	email VARCHAR(50),
-	colegiatura FLOAT,
-	fecha_incorporacion DATETIME,
-	carrera_id INT,
-	tutor_id INT
-);
+IF OBJECT_ID(N'dbo.carreras', N'U') IS NULL
+BEGIN
+	CREATE TABLE carreras (
+		id INT,
+		carrera VARCHAR(100),
+		fecha_alta DATETIME,
+		vigente BIT
+	)
+END;
+
+IF OBJECT_ID(N'dbo.alumnos', N'U') IS NULL
+BEGIN
+	create table alumnos (
+		id INT,
+		nombre VARCHAR(50),
+		apellido VARCHAR(50),
+		email VARCHAR(50),
+		colegiatura FLOAT,
+		fecha_incorporacion DATETIME,
+		carrera_id INT,
+		tutor_id INT
+	)
+END;
